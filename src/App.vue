0e296cb570fffa6b10455d88b34c59fb24c16ead
Sebastian <sebastian@stryczek.pl>
2025-09-28 19:14:01 +0200

    Initialise project

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+import Canvas from "./components/Canvas.vue";
+</script>
+
+<template>
+  <div class="app-container">
+    <Canvas />
+  </div>
+</template>
+
+<style scoped>
+.app-container {
+  box-sizing: border-box;
+  height: 100%;
+  padding: 32px;
+}
+</style>
