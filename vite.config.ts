898c3157a15cc1b81d08a9fcdc0d3ab4e38b6b05
Sebastian <sebastian@stryczek.pl>
2025-10-02 00:47:19 +0200

    Fix base path for github pages

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import vue from "@vitejs/plugin-vue";
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: "/test-task__docusketch/",
   plugins: [vue()],
   server: {
     port: 3000,
