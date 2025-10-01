1b3d495be68c2c5bfcd7851ac1322593774a7676
Sebastian <sebastian@stryczek.pl>
2025-10-02 00:35:13 +0200

    Add readme

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,5 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -7,5 +7,5 @@ export default defineConfig({
   server: {
     port: 3000,
     open: true,
-  }
+  },
 });
